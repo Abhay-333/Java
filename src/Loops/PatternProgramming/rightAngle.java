28fc7e37d3aab4ada29cd797bc1329f881ee556e
Abhay Dhaneshwar <abhayd0331@gmail.com>
2024-12-01 17:43:53 +0530

    Do while and switch done moving towards nested loops

---
+++ src/Loops/PatternProgramming/rightAngle.java
@@ -0,0 +1,5 @@
+package Loops.PatternProgramming;
+
+public class rightAngle {
+
+}
---
+++ src/Loops/PatternProgramming/square.java
@@ -0,0 +1,14 @@
+package Loops.PatternProgramming;
+
+public class square {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int row = 1; row <= n; row++) {
+            for (int column = 1; column <= n; column++) {
+                System.out.print("*");
+            }
+                System.out.println();
+        }
+
+    }
+}
