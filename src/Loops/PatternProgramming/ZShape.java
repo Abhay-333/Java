a066c0bb18225a5e05bfe02bc468b028414057df
Abhay Dhaneshwar <abhayd0331@gmail.com>
2024-12-07 21:34:47 +0530

    Done with z shape going for boat

---
+++ src/Loops/PatternProgramming/ZShape.java
@@ -0,0 +1,19 @@
+package Loops.PatternProgramming;
+
+public class ZShape {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= n; j++) {
+                if (i == n || j == i || i == 1 ) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+}
