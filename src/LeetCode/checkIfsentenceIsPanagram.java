e948d074c6a26aabce3f2ec6f60bacd1bec78c2b
Abhay Dhaneshwar <abhayd0331@gmail.com>
2025-03-13 22:44:22 +0530

    sentence Panagram or not question solved

---
+++ src/LeetCode/checkIfsentenceIsPanagram.java
@@ -0,0 +1,35 @@
+package LeetCode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class checkIfsentenceIsPanagram {
+    public static void main(String[] args) {
+        String sentence = "thequickbrownfoxjumpsoverthelazydog";
+        char[] sentenceArr = sentence.toCharArray();
+
+        // Brute Force: time -> o(n), space -> o(n) n for sentenceArr, m for map1
+//        sentenceArr[]: Takes O(n) space.
+//                map1: Holds 26 key-value pairs → O(m) = O(26) = O(1).
+//                Total: O(n + 1) → O(n). ✅
+
+        Map<Character, Integer> map1 = new HashMap<>();
+
+        for (char i = 'a'; i <= 'z'; i++) {     //This loop runs 26 times (constant, since 'a' to 'z' is always 26).
+            map1.put(i, map1.getOrDefault(i, 0));
+        }
+
+        for (int i = 0; i < sentenceArr.length; i++) {      // loop runs till n
+            if(map1.containsKey(sentenceArr[i])){
+                int value = map1.get(sentenceArr[i]);
+                ++value;
+                map1.put(sentenceArr[i], value);
+            }
+        }
+
+        if(map1.containsValue(0)){
+            System.out.println(false);
+        }
+        System.out.println(map1);
+    }
+}
--- src/LeetCode/jewelsAndStones.java
+++ src/LeetCode/jewelsAndStones.java
@@ -1,7 +1,28 @@
 package LeetCode;
 
+import java.util.Arrays;
+
 public class jewelsAndStones {
     public static void main(String[] args) {
+        String jewels = "aA";
+        String stones = "aAAbbbb";
+
+        char[] jewelsArr = jewels.toCharArray();
+        char[] stonesArr = stones.toCharArray();
+        int counter = 0;
+        System.out.println(Arrays.toString(jewelsArr));
+        System.out.println(Arrays.toString(stonesArr));
+
+        // Brute force Solution: time complexity -> o(n^2), space complexity -> o(n + m) where n = jewelsArr, m =
+        // stonesArr and o(1) for counter;
+        for (int i = 0; i < jewelsArr.length; i++) {
+            for (int j = 0; j < stonesArr.length; j++) {
+                if(jewelsArr[i] == stonesArr[j]){
+                    counter++;
+                }
+            }
+        }
+        System.out.println(counter);
 
     }
 }
